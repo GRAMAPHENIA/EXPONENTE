bc951d582e71321547ee7873a4912ca30a123a21
gramaphenia <dicoratojuanpablo@gmail.com>
2024-01-29 18:35:29 -0300

    mods

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center lg:justify-start  pt-10 px-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-amber-50  text-md lg:flex">
-        <ul className="fixed antialiased md:subpixel-antialiased left-0 top-0 flex w-full justify-between  border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-2 lg:dark:bg-zinc-800/30 gap-4">
+        <ul className="fixed antialiased md:subpixel-antialiased left-0 top-0 flex w-full justify-between  border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-2 lg:dark:bg-zinc-800/30">
           <li className="cursor-pointer">
             <div className="image-container ml-10 block lg:hidden">
               <Image
@@ -16,13 +16,13 @@ export default function Home() {
               />
             </div>
           </li>
-          <li className="hidden lg:block text-gray-300 hover:text-[#aedbfc] hover:bg-gray-800/50 px-4 py-2 rounded-lg cursor-pointer">
-            Desarrollo
-          </li>
-          <li className="hidden lg:block text-gray-300 hover:text-[#aedbfc] hover:bg-gray-800/50 px-4 py-2 rounded-lg cursor-pointer">
+          <li className="hidden lg:block text-gray-300 hover:text-[#aedbfc] hover:bg-gray-800/50 px-4 py-2 rounded-lg cursor-pointer mr-2">
             Creaciones
           </li>
-          <li className="hidden lg:block text-gray-300 hover:text-[#aedbfc] hover:bg-gray-800/50 px-4 py-2 rounded-lg cursor-pointer">
+          <li className="hidden lg:block text-gray-300 hover:text-[#aedbfc] hover:bg-gray-800/50 px-4 py-2 rounded-lg cursor-pointer mr-2">
+            Desarrollo
+          </li>
+          <li className="hidden lg:block text-gray-300 hover:text-[#aedbfc] hover:bg-gray-800/50 px-4 py-2 rounded-lg cursor-pointer mr-2">
             Estudios
           </li>
           <li className="cursor-pointer">

1dbcb44f560cc62b858d641b0faeba0f17f9d080
gramaphenia <dicoratojuanpablo@gmail.com>
2024-04-02 11:39:55 -0300

    mods

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Fang{o}",
+  title: "Di corato Juan",
   description: "Creaciones gramaphenicas",
 };
 
